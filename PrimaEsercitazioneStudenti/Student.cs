4c7e2424116e9e50cd2dab9f9d6937b63c906f38
MaNnu81 <mannuspamm@gmail.com>
2025-05-07 13:24:34 +0200

    ok finito bravo lOre

---
+++ PrimaEsercitazioneStudenti/Student.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PrimaEsercitazioneStudenti
+{
+    internal class Student
+    {
+        public string Name { get; set; } = string.Empty; // Fix for CS8618    
+        public string Surname { get; set; } = string.Empty; // Fix for CS8618    
+        public string Gender { get; set; } = string.Empty; // Fix for CS8618    
+
+        private int _yob;
+        public int Yob
+        {
+            get => _yob;
+            set
+            {
+                if (value < 1920)
+                {
+                    _yob = 1920;
+                }
+                else
+                {
+                    _yob = value;
+                }
+            }
+        }
+        public int[] Marks { get; set; }
+
+        public Student(string name, string surname, string gender, int yob, int[] marks)
+        {
+            Name = name;
+            Surname = surname;
+            Gender = gender;
+            Yob = yob;
+            Marks = marks;
+        }
+
+        public double CalculateMean()
+        {
+            int sum = 0;
+            foreach (var mark in Marks)
+            {
+                sum += mark;
+            }
+            return (double)sum / Marks.Length;
+        }
+
+        public int CalculateAge()
+        {
+            DateTime currentDate = DateTime.Now;
+            int currentYear = currentDate.Year;
+            return currentYear - Yob;
+        }
+    }
+}
